498793062f7909c268ebdd1da5d28cc01ea17120
Miran L <miran@neworigin.io>
2018-11-12 15:53:54 +0100

    Updates

---
+++ precache-manifest.4d84efc28c6755f0b2d30d316f115401.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca49bba4ee5bc19ee6dc0545e112b3e8",
+    "url": "/mobxembe/static/media/bars.ca49bba4.svg"
+  },
+  {
+    "revision": "48088eca235f30e9d096",
+    "url": "/mobxembe/static/js/runtime~main.48088eca.js"
+  },
+  {
+    "revision": "ae5f53719d2441746fff",
+    "url": "/mobxembe/static/js/main.ae5f5371.chunk.js"
+  },
+  {
+    "revision": "7134279b0e65db163cf9",
+    "url": "/mobxembe/static/js/1.7134279b.chunk.js"
+  },
+  {
+    "revision": "ae5f53719d2441746fff",
+    "url": "/mobxembe/static/css/main.53d0c88b.chunk.css"
+  },
+  {
+    "revision": "7134279b0e65db163cf9",
+    "url": "/mobxembe/static/css/1.323fe68e.chunk.css"
+  },
+  {
+    "revision": "6ecc112b3d440a92e9f59312cc34a7da",
+    "url": "/mobxembe/index.html"
+  }
+];
