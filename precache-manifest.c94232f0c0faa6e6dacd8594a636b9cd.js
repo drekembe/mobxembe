9472e3dbf27473dfdb00568cddcd9850b2506418
Miran L <miran@neworigin.io>
2018-11-13 13:43:49 +0100

    Updates

---
+++ precache-manifest.c94232f0c0faa6e6dacd8594a636b9cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca49bba4ee5bc19ee6dc0545e112b3e8",
+    "url": "/mobxembe/static/media/bars.ca49bba4.svg"
+  },
+  {
+    "revision": "48088eca235f30e9d096",
+    "url": "/mobxembe/static/js/runtime~main.48088eca.js"
+  },
+  {
+    "revision": "cd2da8d6bede476bcd44",
+    "url": "/mobxembe/static/js/main.cd2da8d6.chunk.js"
+  },
+  {
+    "revision": "eaf3023be7bc8065c53b",
+    "url": "/mobxembe/static/js/1.eaf3023b.chunk.js"
+  },
+  {
+    "revision": "cd2da8d6bede476bcd44",
+    "url": "/mobxembe/static/css/main.8334ed0c.chunk.css"
+  },
+  {
+    "revision": "eaf3023be7bc8065c53b",
+    "url": "/mobxembe/static/css/1.7fd3a741.chunk.css"
+  },
+  {
+    "revision": "9750203bc12133b4323104c3a11f62ef",
+    "url": "/mobxembe/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mobxembe/precache-manifest.5e125ff542090db62451a410336b5dcc.js"
+  "/mobxembe/precache-manifest.c94232f0c0faa6e6dacd8594a636b9cd.js"
 );
 
 workbox.clientsClaim();
